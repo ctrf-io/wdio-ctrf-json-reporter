683189ff6d9036df6a9f164c7997cf6d14722625
Matthew Thomas <matthewthomasgb@gmail.com>
2023-12-20 19:56:57 +0000

    first commit

---
+++ README.md
@@ -0,0 +1,65 @@
+# Webdriverio JSON Reporter - CTRF
+
+A WDIO test reporter to generate JSON test reports that are [CTRF](https://ctrf.io) compliant.
+
+[Common Test Report Format](https://ctrf.io) helps you generate consistent JSON reports that are agnostic of specific programming languages or test frameworks.
+
+## Features
+
+- Generate JSON test reports that are [CTRF](https://ctrf.io) compliant
+- Straightforward integration with WDIO
+
+## What is CTRF?
+
+A JSON test report schema that is the same structure, no matter which testing tool is used. It's created to provide consistent test reporting agnostic of specific programming languages or testing frameworks. Where many testing frameworks exist, each generating JSON reports in their own way, CTRF provides a standardised schema helping you generate the same report anywhere.
+
+## Installation
+
+```bash
+npm install --save-dev wdio-ctrf-json-reporter
+```
+
+Add the reporter to your wdio.config.ts/js file:
+
+```javascript
+   reporters: [
+        ['ctrf-json', {}]],
+```
+
+Run your tests:
+
+```bash
+npm run wdio
+```
+
+You'll find a JSON file named `ctrf-report.json` in the `ctrf` directory.
+
+## Reporter Options
+
+The reporter supports several configuration options:
+
+```javascript
+reporter: [
+    ['wdio-ctrf-json-reporter', {
+        outputFile: 'custom-name.json', // Optional: Output file name. Defaults to 'ctrf-report.json'.
+        outputDir: 'custom-directory',  // Optional: Output directory path. Defaults to 'ctrf'.
+        appName: 'MyApp',               // Optional: Specify the name of the application under test.
+        appVersion: '1.0.0',            // Optional: Specify the version of the application under test.
+        osPlatform: 'linux',            // Optional: Specify the OS platform.
+        osRelease: '18.04',             // Optional: Specify the OS release version.
+        osVersion: '5.4.0',             // Optional: Specify the OS version.
+        buildName: 'MyApp Build',       // Optional: Specify the build name.
+        buildNumber: '100',             // Optional: Specify the build number.
+    }]
+  ],
+```
+
+## Test Object Properties
+
+The test object in the report includes the following [CTRF properties](https://ctrf.io/docs/schema/test):
+
+| Name       | Type   | Required | Details                                                                             |
+| ---------- | ------ | -------- | ----------------------------------------------------------------------------------- |
+| `name`     | String | Required | The name of the test.                                                               |
+| `status`   | String | Required | The outcome of the test. One of: `passed`, `failed`, `skipped`, `pending`, `other`. |
+| `duration` | Number | Required | The time taken for the test execution, in milliseconds.                             |
---
+++ src/generate-report.ts
@@ -0,0 +1,191 @@
+import WDIOReporter, { type TestStats } from '@wdio/reporter'
+import {
+  type CtrfEnvironment,
+  type CtrfReport,
+  type CtrfTestState,
+} from '../types/ctrf'
+import fs = require('fs')
+import path = require('path')
+
+interface ReporterConfigOptions {
+  outputFile?: string
+  outputDir?: string
+  minimal?: boolean
+  screenshot?: boolean
+  testType?: string
+  appName?: string | undefined
+  appVersion?: string | undefined
+  osPlatform?: string | undefined
+  osRelease?: string | undefined
+  osVersion?: string | undefined
+  buildName?: string | undefined
+  buildNumber?: string | undefined
+}
+
+class GenerateCtrfReport extends WDIOReporter {
+  private readonly ctrfReport: CtrfReport
+  readonly ctrfEnvironment: CtrfEnvironment
+  private readonly reporterConfigOptions: ReporterConfigOptions
+
+  private readonly reporterName = 'wdio-ctrf-json-reporter'
+  private readonly defaultOutputFile = 'ctrf-report.json'
+  private readonly defaultOutputDir = 'ctrf'
+
+  constructor(reporterOptions: ReporterConfigOptions) {
+    super(reporterOptions)
+
+    this.reporterConfigOptions = {
+      outputFile: reporterOptions?.outputFile ?? this.defaultOutputFile,
+      outputDir: reporterOptions?.outputDir ?? this.defaultOutputDir,
+      appName: reporterOptions?.appName ?? undefined,
+      appVersion: reporterOptions?.appVersion ?? undefined,
+      osPlatform: reporterOptions?.osPlatform ?? undefined,
+      osRelease: reporterOptions?.osRelease ?? undefined,
+      osVersion: reporterOptions?.osVersion ?? undefined,
+      buildName: reporterOptions?.buildName ?? undefined,
+      buildNumber: reporterOptions?.buildNumber ?? undefined,
+    }
+    this.ctrfReport = {
+      results: {
+        tool: {
+          name: 'webdriverio',
+        },
+        summary: {
+          tests: 0,
+          passed: 0,
+          failed: 0,
+          skipped: 0,
+          pending: 0,
+          other: 0,
+          start: 0,
+          stop: 0,
+        },
+        tests: [],
+      },
+    }
+
+    this.ctrfEnvironment = {}
+
+    if (this.reporterConfigOptions?.outputFile !== undefined)
+      this.setFilename(this.reporterConfigOptions.outputFile)
+
+    if (
+      !fs.existsSync(
+        this.reporterConfigOptions.outputDir ?? this.defaultOutputDir
+      )
+    ) {
+      fs.mkdirSync(
+        this.reporterConfigOptions.outputDir ?? this.defaultOutputDir,
+        { recursive: true }
+      )
+    }
+  }
+
+  onRunnerStart(): void {
+    this.ctrfReport.results.summary.start = Date.now()
+    this.setEnvironmentDetails(this.reporterConfigOptions ?? {})
+    if (this.hasEnvironmentDetails(this.ctrfEnvironment)) {
+      this.ctrfReport.results.environment = this.ctrfEnvironment
+    }
+  }
+
+  onTestEnd(testStats: TestStats): void {
+    this.updateCtrfTestResultsFromTestStats(testStats, testStats.state)
+    this.updateCtrfTotalsFromTestStats(testStats)
+  }
+
+  onRunnerEnd(): void {
+    this.ctrfReport.results.summary.stop = Date.now()
+    this.writeReportToFile(this.ctrfReport)
+    console.log(JSON.stringify(this.reporterConfigOptions))
+  }
+
+  private setFilename(filename: string): void {
+    if (filename.endsWith('.json')) {
+      this.reporterConfigOptions.outputFile = filename
+    } else {
+      this.reporterConfigOptions.outputFile = `${filename}.json`
+    }
+  }
+
+  private updateCtrfTotalsFromTestStats(testStats: TestStats): void {
+    this.ctrfReport.results.summary.tests += 1
+
+    switch (testStats.state) {
+      case 'passed':
+        this.ctrfReport.results.summary.passed += 1
+        break
+      case 'failed':
+        this.ctrfReport.results.summary.failed += 1
+        break
+      case 'skipped':
+        this.ctrfReport.results.summary.skipped += 1
+        break
+      case 'pending':
+        this.ctrfReport.results.summary.pending += 1
+        break
+      default:
+        this.ctrfReport.results.summary.other += 1
+        break
+    }
+  }
+
+  private updateCtrfTestResultsFromTestStats(
+    test: TestStats,
+    status: CtrfTestState
+  ): void {
+    this.ctrfReport.results.tests.push({
+      name: test.title,
+      status,
+      duration: test._duration,
+    })
+  }
+
+  setEnvironmentDetails(reporterConfigOptions: ReporterConfigOptions): void {
+    if (reporterConfigOptions.appName !== undefined) {
+      this.ctrfEnvironment.appName = reporterConfigOptions.appName
+    }
+    if (reporterConfigOptions.appVersion !== undefined) {
+      this.ctrfEnvironment.appVersion = reporterConfigOptions.appVersion
+    }
+    if (reporterConfigOptions.osPlatform !== undefined) {
+      this.ctrfEnvironment.osPlatform = reporterConfigOptions.osPlatform
+    }
+    if (reporterConfigOptions.osRelease !== undefined) {
+      this.ctrfEnvironment.osRelease = reporterConfigOptions.osRelease
+    }
+    if (reporterConfigOptions.osVersion !== undefined) {
+      this.ctrfEnvironment.osVersion = reporterConfigOptions.osVersion
+    }
+    if (reporterConfigOptions.buildName !== undefined) {
+      this.ctrfEnvironment.buildName = reporterConfigOptions.buildName
+    }
+    if (reporterConfigOptions.buildNumber !== undefined) {
+      this.ctrfEnvironment.buildNumber = reporterConfigOptions.buildNumber
+    }
+  }
+
+  hasEnvironmentDetails(environment: CtrfEnvironment): boolean {
+    return Object.keys(environment).length > 0
+  }
+
+  private writeReportToFile(data: CtrfReport): void {
+    const filePath = path.join(
+      this.reporterConfigOptions.outputDir ?? this.defaultOutputDir,
+      this.reporterConfigOptions.outputFile ?? this.defaultOutputFile
+    )
+    const str = JSON.stringify(data, null, 2)
+    try {
+      fs.writeFileSync(filePath, str + '\n')
+      console.log(
+        `${this.reporterName}: successfully written ctrf json to %s/%s`,
+        this.reporterConfigOptions.outputDir,
+        this.reporterConfigOptions.outputFile
+      )
+    } catch (error) {
+      console.error(`Error writing ctrf json report:, ${String(error)}`)
+    }
+  }
+}
+
+export default GenerateCtrfReport
