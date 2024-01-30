17a0acd608d504e30bf81279b64bc94ff9391c1c
Matthew Thomas <matthewthomasgb@gmail.com>
2024-01-30 21:45:41 +0000

    chore: format changes

--- release.config.js
+++ release.config.js
@@ -1,18 +1,18 @@
 // release.config.js
 module.exports = {
-    branches: ['main'],
-    plugins: [
-      '@semantic-release/commit-analyzer',
-      '@semantic-release/release-notes-generator',
-      '@semantic-release/changelog',
-      [
-        '@semantic-release/git',
-        {
-          assets: ['CHANGELOG.md', 'package.json'], 
-          message: 'chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}'
-        },
-      ],
-      '@semantic-release/github'
+  branches: ['main'],
+  plugins: [
+    '@semantic-release/commit-analyzer',
+    '@semantic-release/release-notes-generator',
+    '@semantic-release/changelog',
+    [
+      '@semantic-release/git',
+      {
+        assets: ['CHANGELOG.md', 'package.json'],
+        message:
+          'chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}',
+      },
     ],
-  };
-  
+    '@semantic-release/github',
+  ],
+}
